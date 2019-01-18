d5b09bb16e0057d79a31080253427d2e3b502636
franda@gitlab.com <francisco@carreralinux.com.ar>
2019-01-18 11:16:18 -0300

    Upload code

---
+++ minimize.py
@@ -0,0 +1,21 @@
+import os
+import sys
+relativePath = os.getcwd()
+inputPath = sys.argv[1]
+outputPath = sys.argv[2]
+inputFile = open(relativePath + '\\' + inputPath,"r")
+outputFile = open(relativePath + '\\' + outputPath,"w")
+while True:
+	char=inputFile.read(1)
+	if not char: break
+	if char=='\n': char = ''
+	if char==' ': char = ''
+	outputFile.write(char)
+outputFile = open(relativePath + '\\' + outputPath,"r")
+inputSize = os.fstat(inputFile.fileno()).st_size
+outputSize = os.fstat(outputFile.fileno()).st_size
+print("Input size: ", end='');
+print(inputSize)
+print("Output size: ", end='');
+print(outputSize)
+print(relativePath + '\\' + outputPath)
