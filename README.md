d5d1d3b0eb2a2140d1953513f28d1276d363910e
franda@gitlab.com <francisco@carreralinux.com.ar>
2019-01-18 11:48:51 -0300

    Fixed white space removal and updated readme

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # miniFile
 miniFile is a python script for minimizing CSS/Html/Javascript(untested)
+
+## Usage
+To use miniFile simply type:
+py minimize.py inputfile outputfile [t/f]
+
+with t you declare you wish for minimize to remove duplicated whitespaces, if you do not wish for this functionality simply use f
--- minimize.py
+++ minimize.py
@@ -6,14 +6,16 @@ outputPath = sys.argv[2]
 removeSpace = sys.argv[3]
 inputFile = open(relativePath + '\\' + inputPath,"r")
 outputFile = open(relativePath + '\\' + outputPath,"w")
+lastChar = ''
 while True:
 	char=inputFile.read(1)
 	if not char: break
 	if char=='\n': char = ''
 	if char=='\t': char = ''
 	if char=='\v': char = ''
-	if removeSpace=="t" and char==' ': char = ''
+	if removeSpace=="t" and lastChar==' ' and char == ' ': char = ''
 	outputFile.write(char)
+	lastChar = char
 outputFile = open(relativePath + '\\' + outputPath,"r")
 inputSize = os.fstat(inputFile.fileno()).st_size
 outputSize = os.fstat(outputFile.fileno()).st_size
